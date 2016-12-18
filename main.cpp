d51380b8ec3b3ed959067b5a6c9d82195c0f61c4
baiysh-san <baiysh.toktosunov@gmail.com>
2016-12-18 23:42:33 +0600

    commit m

--- main.cpp
+++ main.cpp
@@ -9,6 +9,24 @@ int main() {
     Mat gray=imread("/home/muhila/ClionProjects/HIstTest/lena.jpg",0);
     namedWindow( "Gray", 1 );
     imshow( "Gray", gray );
+
+    // Инициализация параметров
+    int histSize = 256;
+    float range[] = { 0, 255 };
+    const float *ranges[] = { range };
+
+    // Гистограмма
+    MatND hist;
+    calcHist( &gray, 1, 0, Mat(), hist, 1, &histSize, ranges, true, false );
+
+    // Показать подсчитанную гистограмму
+    double total;
+    total = gray.rows * gray.cols;
+    for( int h = 0; h < histSize; h++ )
+    {
+        float binVal = hist.at<float>(h);
+        cout<<" "<<binVal;
+    }
     waitKey(0);
     return 0;
     //commit
