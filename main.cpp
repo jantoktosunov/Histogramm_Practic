6c28d30153fec8ad97c61db0c7802fa4aa0d434a
muhila <darkfuria96@mail.ru>
2016-12-18 23:35:38 +0600

    name

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace cv;
 int main() {
-    Mat gray=imread("/home/tokjan/ClionProjects/HIstTest/lena.jpg",0);
+    Mat gray=imread("/home/muhila/ClionProjects/HIstTest/lena.jpg",0);
     namedWindow( "Gray", 1 );
     imshow( "Gray", gray );
     waitKey(0);
