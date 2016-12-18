50ec35a234084686ff68c081a6dc5954fb2c57a4
Zhanysh <arcsinner@mail.ru>
2016-12-18 23:44:33 +0600

    График

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace cv;
 int main() {
-    Mat gray=imread("/home/muhila/ClionProjects/HIstTest/lena.jpg",0);
+    Mat gray=imread("/home/tokjan/ClionProjects/HIstTest/lena.jpg",0);
     namedWindow( "Gray", 1 );
     imshow( "Gray", gray );
 
@@ -27,7 +27,35 @@ int main() {
         float binVal = hist.at<float>(h);
         cout<<" "<<binVal;
     }
+    // График
+    int hist_w = 512; int hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+
+    Mat histImage( hist_h, hist_w, CV_8UC1, Scalar( 0,0,0) );
+    normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+    for( int i = 1; i < histSize; i++ )
+    {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1)) ) ,
+              Point( bin_w*(i), hist_h - cvRound(hist.at<float>(i)) ),
+              Scalar( 255, 0, 0), 2, 8, 0  );
+    }
+    for(int i =0; i < gray.rows; i++){
+        for(int k = 0; k <gray.cols; k ++){
+
+            if(gray.at<uchar>(i,k)<=25){
+                gray.at<uchar>(i,k)=(gray.at<uchar>(i,k))*0.45;
+            }
+
+
+        }
+
+    }
+
+
+    namedWindow( "Result", 1 );
+    imshow( "Result", histImage );
     waitKey(0);
     return 0;
-    //commit
+
 }
